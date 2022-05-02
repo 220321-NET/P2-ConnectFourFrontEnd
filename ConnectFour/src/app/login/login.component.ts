f467c03954ddc46bf9415d254df603ac48c8d1d7
Jose Mendoza <mendozaj9486@gmail.com>
2022-05-02 14:36:49 -0500

    Simple login and signup

--- ConnectFour/src/app/login/login.component.ts
+++ ConnectFour/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { player } from '../models/player';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  Player: player = {
+    PlayerID: -1,
+    Email: '',
+    Username: '',
+    Password: '',
+    Wins: -1,
+    Losses: -1,
+    Ties: -1
+  }
+
+  constructor(private api: HttpService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  submitForm() {
+    this.api.getPlayer(this.Player.Username).subscribe((res) => {
+      this.router.navigate(['home'])
+    })
+  }
 }
