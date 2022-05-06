86d89b613ef2d101d39f399f26265fccdc39f32f
Jose Mendoza <mendozaj9486@gmail.com>
2022-05-06 12:33:13 -0500

    Added win logic, login modal, winner modal, and database persistence

--- ConnectFour/src/app/board/board.component.ts
+++ ConnectFour/src/app/board/board.component.ts
@@ -1,8 +1,12 @@
-import { NoopAnimationPlayer } from '@angular/animations';
-import { Component, OnInit, Output } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
+import { InGameLoginComponent } from '../in-game-login/in-game-login.component';
+import { board } from '../models/board';
+import { lobby } from '../models/lobby';
 import { player } from '../models/player';
 import { HttpService } from '../services/http.service';
+import { WinnerComponent } from '../winner/winner.component';
 
 
 @Component({
@@ -12,10 +16,12 @@ import { HttpService } from '../services/http.service';
 })
 export class BoardComponent implements OnInit {
 
+  modalRef: MdbModalRef<WinnerComponent> | null = null;
+
   currentUser: string = '';
 
   Player1: player = {
-    PlayerID: 0,
+    PlayerID: -10,
     Email: '',
     Username: '',
     Password: '',
@@ -26,7 +32,7 @@ export class BoardComponent implements OnInit {
   Player1color: string = 'red';
 
   Player2: player = {
-    PlayerID: 0,
+    PlayerID: -10,
     Email: '',
     Username: '',
     Password: '',
@@ -36,7 +42,22 @@ export class BoardComponent implements OnInit {
   }
   Player2color: string = 'yellow';
 
+  Lobby: lobby = {
+    LobbyID: 0,
+    Player1ID: 0,
+    Player2ID: 0
+  }
+
+  Board: board = {
+    BoardID: 0,
+    WinID: -5,
+    LobbyID: 0
+  }
+
+  opacity: string = '100%';
   currentPlayer: number = -1;
+  winner: number = -1;
+  winnerName: string = '';
 
   board: number[][] = [
     [-1, -1, -1, -1, -1, -1, -1],
@@ -47,23 +68,77 @@ export class BoardComponent implements OnInit {
     [-1, -1, -1, -1, -1, -1, -1]
   ]
 
-  constructor(private router: ActivatedRoute, private api: HttpService) {
+  constructor(private route: Router, private router: ActivatedRoute, private api: HttpService, private modalService: MdbModalService) {
   }
 
   addPiece(column: number): void {
-    for (let r = 0; r < this.board.length; r++) {
-      if (this.board[r][column] != -1 && this.board[0][column] == -1) {
-        if (r != 0) {
-          this.board[r - 1][column] = this.currentPlayer;
-          this.changeTurn()
-          return;
+    this.changeTurn();
+    if (this.winner == -1) {
+      for (let r = 0; r < this.board.length; r++) {
+        if (this.board[r][column] != -1 && this.board[0][column] == -1) {
+          if (r != 0) {
+            // this.changeTurn()
+            this.board[r - 1][column] = this.currentPlayer;
+            this.winner = this.checkWin();
+            if (this.winner != -1) {
+              this.displayWinner(this.winnerName);
+            }
+            return;
+          }
+        }
+        else if (r === this.board.length - 1 && this.board[0][column] == -1) {
+          // this.changeTurn()
+          this.board[r][column] = this.currentPlayer;
+          this.winner = this.checkWin();
+          if (this.winner != -1) {
+            this.displayWinner(this.winnerName);
+          }
         }
       }
-      else if (r === this.board.length - 1 && this.board[0][column]) {
-        this.board[r][column] = this.currentPlayer;
-        this.changeTurn()
-      }
+    } else {
+      this.displayWinner(this.winnerName);
     }
+  }
+
+  displayWinner(winnerName: string): void {
+    this.opacity = "25%";
+    this.modalRef = this.modalService.open(WinnerComponent, {
+      modalClass: 'modal-dialog-centered',
+      data: { winnerName }
+    })
+    this.modalRef.onClose.subscribe((message: any) => {
+      this.opacity = message;
+      this.route.navigate(['home', this.currentUser]);
+    });
+  }
+
+  displayPlayer2Login(): void {
+    this.opacity = "25%";
+    this.modalRef = this.modalService.open(InGameLoginComponent, {
+      modalClass: 'modal-dialog-centered'
+    })
+    this.modalRef.onClose.subscribe((message: any) => {
+      this.Player2 = message;
+      this.opacity = "100%";
+      this.addLobbytoDB();
+    })
+
+  }
+
+  addLobbytoDB(): void {
+    this.Lobby.Player1ID = this.Player1.PlayerID;
+    this.Lobby.Player2ID = this.Player2.PlayerID;
+    this.api.addLobby(this.Lobby).subscribe((res) => {
+      this.Lobby = res;
+      this.addBoardtoDB(res.LobbyID);
+    })
+  }
+
+  addBoardtoDB(lobbyid: number): void {
+    this.Board.LobbyID = lobbyid;
+    this.api.addBoard(this.Board).subscribe((res) => {
+      this.Board = res;
+    })
   }
 
   changeTurn(): void {
@@ -85,6 +160,84 @@ export class BoardComponent implements OnInit {
     return 'white';
   }
 
+  getWinnerName(winner: number): void {
+    if (winner === this.Player1.PlayerID)
+      this.winnerName = this.Player1.Username;
+    else
+      this.winnerName = this.Player2.Username;
+  }
+
+  checkWin(): number {
+    let count = 0;
+
+    // CHECK HORIZONTAL
+    for (let row = this.board.length - 1; row >= 0; row--) {
+      for (let col = 0; col < this.board[row].length - 3; col++) {
+        if (this.board[row][col] === this.currentPlayer) {
+          count = 0;
+          for (let i = 0; i < 4; i++) {
+            if (this.board[row][col] !== this.board[row][col + i])
+              break;
+            else {
+              count++;
+            }
+          }
+          if (count == 4) { this.getWinnerName(this.currentPlayer); return this.currentPlayer; }
+        }
+      }
+    }
+
+    // CHECK VERTICAL
+    for (let row = this.board.length - 1; row >= 3; row--) {
+      for (let col = 0; col < this.board[1].length; col++) {
+        if (this.board[row][col] === this.currentPlayer) {
+          count = 0;
+
+          for (let i = 0; i < 4; i++) {
+            if (this.board[row][col] !== this.board[row - i][col])
+              break;
+            else { count++; }
+          }
+          if (count == 4) { this.getWinnerName(this.currentPlayer); return this.currentPlayer; }
+        }
+      }
+    }
+
+    // CHECK FORWARD DIAGONAL
+    for (let row = this.board.length - 1; row >= 3; row--) {
+      for (let col = 0; col < this.board[row].length - 3; col++) {
+        if (this.board[row][col] === this.currentPlayer) {
+          count = 0;
+
+          for (let i = 0; i < 4; i++) {
+            if (this.board[row][col] !== this.board[row - i][col + i])
+              break;
+            else { count++; }
+          }
+          if (count == 4) { this.getWinnerName(this.currentPlayer); return this.currentPlayer; }
+        }
+      }
+    }
+
+    // CHECK BACKWARD DIAGONAL
+    for (let row = this.board.length - 1; row >= 3; row--) {
+      for (let col = 3; col < this.board[1].length; col++) {
+        if (this.board[row][col] === this.currentPlayer) {
+          count = 0;
+
+          for (let i = 0; i < 4; i++) {
+            if (this.board[row][col] !== this.board[row - i][col - i])
+              break;
+            else { count++; }
+          }
+          if (count == 4) { this.getWinnerName(this.currentPlayer); return this.currentPlayer; }
+        }
+      }
+    }
+
+    return -1;
+  }
+
   ngOnInit(): void {
     this.router.params.subscribe(params => {
       this.currentUser = params['username'];
@@ -92,6 +245,7 @@ export class BoardComponent implements OnInit {
         this.Player1 = res.body!;
       })
     })
+    this.displayPlayer2Login();
   }
 
 }
